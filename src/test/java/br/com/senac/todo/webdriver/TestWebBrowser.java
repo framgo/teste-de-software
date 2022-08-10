59ee9eb69d7c7fbc5762fa5bcc7ea6a8fdb33d8c
framgo <lucasaraujooks@gmail.com>
2022-08-10 20:59:08 -0300

    feat: meio que uma correção do exercicio 03 com mais detalhes

--- src/test/java/br/com/senac/todo/webdriver/TestWebBrowser.java
+++ src/test/java/br/com/senac/todo/webdriver/TestWebBrowser.java
@@ -8,10 +8,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,6 +23,11 @@ public class TestWebBrowser {
 
     private WebDriver driver;
     private WebElement searchBox;
+
+    private WebElement searchButton;
+
+    private Actions actions;
+
     private WebElement searchResults;
 
     @BeforeEach
@@ -70,5 +79,34 @@ public class TestWebBrowser {
 
         assertTrue(searchResults.isDisplayed());
         assertThat(driver.getTitle().startsWith("Turma 2022.3"));
+    }
+
+    @Test
+    // professor deixou o codigo com mais detalhes.
+    void searchSenacRJEX03(){
+        driver.get("https://www.rj.senac.br/");
+
+        searchButton = driver.findElement(By.id("bt-opensearch"));
+
+        actions = new Actions(driver);
+        actions.moveToElement(searchButton).click().perform();
+
+        searchBox = new WebDriverWait(driver, Duration.of(20, ChronoUnit.SECONDS)).until(ExpectedConditions.elementToBeClickable(By.id("search-field")));
+        searchBox.sendKeys("Técnico de informática");
+        searchBox.submit();
+
+        searchButton = driver.findElement(By.className("thumb-busca"));
+        actions.moveToElement(searchButton).click().perform();
+
+        var turma = driver.findElement(By.cssSelector("h1[class='fw-bold color-2-1 flt-left']")).getText();
+
+        assertEquals("Turma 2022.3", turma);
+
+        // xpath -> linguagem de consulta de nós de um documento XML
+        //var turma = driver.findElement(By.xpath("//h1[@class='fw-bold color-2-1 flt-left']")).getText();
+
+        //var turma = driver.findElement(By.cssSelector("h1[class='fw-bold color-2-1 flt-left']")).getAttribute("innerHTML");
+
+        //var nomeCompleto = driver.findElement(By.name("input[name='nome_completo']")).getAttribute("value");
     }
 }
